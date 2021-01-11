1945bb1bc734fd44d8a785519a98fd4fb4691461
konianzero <konianzero@gmail.com>
2021-01-11 19:24:22 +0300

    Add Swagger 2

---
+++ src/main/java/org/restaurant/voting/web/RootController.java
@@ -0,0 +1,15 @@
+package org.restaurant.voting.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import springfox.documentation.annotations.ApiIgnore;
+
+@ApiIgnore
+@Controller
+public class RootController {
+
+    @GetMapping("/")
+    public String root() {
+        return "redirect:swagger-resources";
+    }
+}
